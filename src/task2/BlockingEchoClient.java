e5e67d01707374752442fafe734a1302f548882b
semenov-d <semenov-d@protei-lab.ru>
2026-05-04 01:46:46 +0300

    server-client - Task 2: Blocking Socket, чуть переделал логику.

---
+++ src/task2/BlockingEchoClient.java
@@ -0,0 +1,34 @@
+package task2;
+
+import java.io.*;
+import java.net.*;
+
+public class BlockingEchoClient {
+    public static void main(String[] args) throws IOException {
+        String host = "localhost";
+        int port = 8080;
+        BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+
+        try (
+                Socket socket = new Socket(host, port);
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))
+        ) {
+            System.out.print("You: ");
+            String message = console.readLine();
+            out.println(message);
+            out.flush();
+
+            String response = in.readLine();
+            System.out.println("Server: " + response);
+
+            String confirmation;
+            do {
+                System.out.print("You: ");
+                confirmation = console.readLine();
+                out.println(confirmation);
+                out.flush();
+            } while (!"Hi".equalsIgnoreCase(confirmation));
+        }
+    }
+}
--- src/task2/BlockingEchoServer.java
+++ src/task2/BlockingEchoServer.java
@@ -1,24 +1,30 @@
+package task2;
+
 import java.io.*;
 import java.net.*;
 
-public class Server {
+public class BlockingEchoServer {
     public static void main(String[] args) throws IOException {
-        int port = 8081;
+        int port = 8080;
         ServerSocket serverSocket = new ServerSocket(port);
 
         while (true) {
             Socket clientSocket = serverSocket.accept();
+
             try (
                     BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
                     PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true)
             ) {
                 String greeting = in.readLine();
-                if ("Hello".equals(greeting)) {
+
+                if ("Hello".equalsIgnoreCase(greeting)) {
                     out.println("Hi");
                     out.flush();
-                    String confirmation = in.readLine();
-                    if (!"Hi".equals(confirmation)) {
-                        break;
+
+                    while (!"Hi".equalsIgnoreCase(in.readLine())) {
+                    }
+                } else {
+                    while (!"Hi".equalsIgnoreCase(in.readLine())) {
                     }
                 }
             } finally {
